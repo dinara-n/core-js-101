57052f1d22ad7e818857fd3d6ea3e4b0fc761df4
Dinara <dinaranav1987@gmail.com>
2022-11-26 03:52:51 +0400

    feat: solve isPrime in Numbers tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -210,8 +210,20 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  for (let i = 2; i <= n; i += 1) {
+    if (n !== i && n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+  // throw new Error('Not implemented');
 }
 
 /**
