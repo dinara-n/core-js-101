77fb8677c3125e212f29e4b9cb13298eb848d240
Dinara <dinaranav1987@gmail.com>
2022-11-26 15:31:28 +0400

    feat: solve getFalsyValuesCount in Arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -398,8 +398,9 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((elem) => !elem).length;
+  // throw new Error('Not implemented');
 }
 
 /**
