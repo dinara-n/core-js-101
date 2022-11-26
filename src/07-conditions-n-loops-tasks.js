9038789060f07e9f88462c0950ab62761623c3ad
Dinara <dinaranav1987@gmail.com>
2022-11-26 21:06:10 +0400

    feat: solve reverseString in ConditionsNLoops tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -263,8 +263,9 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
+  // throw new Error('Not implemented');
 }
 
 
