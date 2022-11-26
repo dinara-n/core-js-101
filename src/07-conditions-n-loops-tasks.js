02ad2eca94bea8a0cd35f66d9e631a04238c705f
Dinara <dinaranav1987@gmail.com>
2022-11-26 21:34:44 +0400

    feat: solve isCreditCardNumber in ConditionsNLoops tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -307,8 +307,17 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const arr = ccn.toString().split('').reverse();
+  for (let i = 1; i < arr.length; i += 2) {
+    let newElem = arr[i] * 2;
+    if (newElem > 9) {
+      newElem -= 9;
+    }
+    arr[i] = newElem;
+  }
+  return arr.reduce((sum, elem) => sum + +elem, 0) % 10 === 0;
+  // throw new Error('Not implemented');
 }
 
 /**
