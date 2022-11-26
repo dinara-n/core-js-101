c5a09582c4a4910f4580a0f4876175921c74fbaf
Dinara <dinaranav1987@gmail.com>
2022-11-26 14:31:39 +0400

    feat: solve getMovingSum in Arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -256,8 +256,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let addedAmount = 0;
+  return arr.map((elem, i) => {
+    if (i !== 0) {
+      addedAmount += arr[i - 1];
+    }
+    return elem + addedAmount;
+  });
+  // throw new Error('Not implemented');
 }
 
 /**
