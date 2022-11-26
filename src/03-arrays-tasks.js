e60dfbea643e620712bafd19dd4ddf0457391761
Dinara <dinaranav1987@gmail.com>
2022-11-26 15:21:37 +0400

    feat: solve getPositivesCount in Arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -341,8 +341,9 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((elem) => (typeof elem === 'number') && (elem > 0)).length;
+  // throw new Error('Not implemented');
 }
 
 /**
