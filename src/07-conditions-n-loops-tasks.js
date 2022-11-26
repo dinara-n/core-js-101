452a8c4bce81b56ccbac1ac4d909440db2844419
Dinara <dinaranav1987@gmail.com>
2022-11-26 20:13:52 +0400

    feat: solve isTriangle in ConditionsNLoops tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -101,8 +101,12 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if ((a >= b + c) || (b >= a + c) || (c >= a + b)) {
+    return false;
+  }
+  return true;
+  // throw new Error('Not implemented');
 }
 
 
