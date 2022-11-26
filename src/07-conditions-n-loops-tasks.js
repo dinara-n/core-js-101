8490fe61463c0d2cdf54a5d999689bacbe3094f3
Dinara <dinaranav1987@gmail.com>
2022-11-26 21:38:33 +0400

    feat: solve getDigitalRoot in ConditionsNLoops tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -334,8 +334,13 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let res = num;
+  while (res > 9) {
+    res = res.toString().split('').reduce((sum, elem) => sum + +elem, 0);
+  }
+  return res;
+  // throw new Error('Not implemented');
 }
 
 
