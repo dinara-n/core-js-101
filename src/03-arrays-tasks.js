ef9d16f3222e6490870bb54866afd0241e331db1
Dinara <dinaranav1987@gmail.com>
2022-11-26 15:30:04 +0400

    feat: solve getItemsSum in Arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -381,8 +381,9 @@ function sortDigitNamesByNumericOrder(arr) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  return arr.reduce((sum, elem) => sum + elem, 0);
+  // throw new Error('Not implemented');
 }
 
 /**
