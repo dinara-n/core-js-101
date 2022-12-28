be82172a317dfc09797231aaaafb0d30fe91508e
Dinara <dinaranav1987@gmail.com>
2022-12-29 01:30:15 +0400

    feat: solve memoize in FunctionsNClosures tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -94,8 +94,10 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const res = func();
+  return () => res;
+  // throw new Error('Not implemented');
 }
 
 
